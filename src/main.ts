1d689c48dba6d1ee3306a4621097be7e922cd2da
Samudra Ajri Kifli <samudra1ajri@gmail.com>
2026-01-10 15:58:43 +0700

    feat: integrate nest-winston for logging and update UserRepository to use logger

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,15 @@ import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
 import mustacheExpress from 'mustache-express';
 import { ConfigService } from '@nestjs/config';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.use(cookieParser('SECRET'));
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const loggerService = app.get(WINSTON_MODULE_NEST_PROVIDER);
+  app.useLogger(loggerService);
 
   app.set('views', __dirname + '/../views');
   app.set('view engine', 'html');
--- src/user/user-repository/user-repository.ts
+++ src/user/user-repository/user-repository.ts
@@ -1,14 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { User } from '@prisma/client';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
 import { PrismaService } from 'src/prisma/prisma/prisma.service';
+import { Logger } from 'winston';
 
 @Injectable()
 export class UserRepository {
-  constructor(private readonly prismaService: PrismaService) {
-    console.info('Create User Repository');
+  constructor(
+    private readonly prismaService: PrismaService,
+    @Inject(WINSTON_MODULE_PROVIDER) private logger: Logger,
+  ) {
+    this.logger.info('Create User Repository');
   }
 
   async save(firstName: string, lastName?: string): Promise<User> {
+    this.logger.info(
+      `Saving user with firstName: ${firstName} and lastName: ${lastName}`,
+    );
     return await this.prismaService.user.create({
       data: { firstName, lastName },
     });
