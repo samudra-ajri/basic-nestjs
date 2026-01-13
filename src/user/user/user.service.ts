0709aae79b61fac023d6f36c17022ee60e5fe278
Samudra Ajri Kifli <samudra1ajri@gmail.com>
2026-01-13 22:25:08 +0700

    feat: add validation module and service with Zod integration for input validation

--- src/user/user/user.service.ts
+++ src/user/user/user.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { ValidationService } from 'src/validation/validation/validation.service';
+import z from 'zod';
 
 @Injectable()
 export class UserService {
+  constructor(private validationService: ValidationService) {}
+
   sayHello(firstName: string, lastName: string): string {
-    return `Hello ${firstName} ${lastName}`;
+    const schema = z.string().min(3).max(50);
+    const validFirstName = this.validationService.validate(schema, firstName);
+    const validLastName = this.validationService.validate(schema, lastName);
+    return `Hello ${validFirstName} ${validLastName}`;
   }
 }
